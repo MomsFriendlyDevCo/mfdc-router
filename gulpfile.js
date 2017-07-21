209aaff3f924404938d9ae0b5dfd49069ba662a6
Matt Carter <m@ttcarter.com>
2017-07-21 15:16:49 +1000

    NPM update REFACTOR: NPM switch gulp-annotate -> babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-var annotate = require('gulp-ng-annotate');
 var babel = require('gulp-babel');
 var gulp = require('gulp');
 var fs = require('fs');
@@ -8,8 +7,25 @@ var replace = require('gulp-replace');
 var uglify = require('gulp-uglify');
 
 gulp.task('default', ['build']);
+gulp.task('build', ['build:mfdc-router', 'build:angular-mfdc-router']);
 
-gulp.task('build', function () {
+gulp.task('build:mfdc-router', function() {
+	gulp.src('./src/mfdc-router.js')
+		.pipe(rename('mfdc-router.js'))
+		.pipe(preprocess({
+			includeBase: __dirname,
+			context: {},
+		}))
+		.pipe(babel({
+			presets: ['es2015'],
+		}))
+		.pipe(gulp.dest('./dist'))
+		.pipe(rename('mfdc-router.min.js'))
+		.pipe(uglify({mangle: false}))
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build:angular-mfdc-router', function () {
 	gulp.src('./src/angular-mfdc-router.js')
 		.pipe(rename('angular-mfdc-router.js'))
 		.pipe(preprocess({
@@ -21,11 +37,13 @@ gulp.task('build', function () {
 		.pipe(replace(/\n{3,}/g, '\n'))
 		.pipe(replace(/setTimeout\(/, '$timeout('))
 		.pipe(replace(/\$q.promise/g, '$q'))
-		.pipe(babel({presets: ['es2015']}))
+		.pipe(babel({
+			presets: ['es2015'],
+			plugins: ['angularjs-annotate'],
+		}))
 		.pipe(replace(/^'use strict';$/gm, ''))
 		.pipe(gulp.dest('./dist'))
 		.pipe(rename('angular-mfdc-router.min.js'))
-		.pipe(annotate())
 		.pipe(uglify({mangle: false}))
 		.pipe(gulp.dest('./dist'));
 });
